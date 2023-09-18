9762dcd8d6c0415ec70fa3379b27adfec96415d3
Wenhan Li <wenhan@Wenhans-MacBook-Air.local>
2023-09-17 21:31:57 -0400

    task 1

--- VM_Scaling_Project/python/task1/horizontal-scaling.py
+++ VM_Scaling_Project/python/task1/horizontal-scaling.py
@@ -8,12 +8,15 @@ import json
 import configparser
 import re
 from dateutil.parser import parse
+import datetime
+from bs4 import BeautifulSoup
+
 
 
 ########################################
 # Constants
 ########################################
-with open('horizontal-scaling-config.json') as file:
+with open('python/task1/horizontal-scaling-config.json') as file:
     configuration = json.load(file)
 
 LOAD_GENERATOR_AMI = configuration['load_generator_ami']
@@ -24,13 +27,30 @@ INSTANCE_TYPE = configuration['instance_type']
 SUBMISSION_USERNAME = os.environ['SUBMISSION_USERNAME']
 SUBMISSION_PASSWORD = os.environ['SUBMISSION_PASSWORD']
 
+ec2 = boto3.resource('ec2', region_name='us-east-1')
+
 ########################################
 # Tags
 ########################################
-tag_pairs = [
+
+def convert_to_tags(tag_pairs):
+    return [{'Key': k, 'Value': v} for k, v in tag_pairs]
+
+# Load Generator Tags
+LG_TAG_PAIRS = [
     ("Project", "vm-scaling"),
+    ("Name", "P1 load generator")
 ]
-TAGS = [{'Key': k, 'Value': v} for k, v in tag_pairs]
+
+# Web Service Tags
+WS_TAG_PAIRS = [
+    ("Project", "vm-scaling"),
+    ("Name", "P1 web service")
+]
+
+LG_TAGS = convert_to_tags(LG_TAG_PAIRS)
+WS_TAGS = convert_to_tags(WS_TAG_PAIRS)
+
 
 TEST_NAME_REGEX = r'name=(.*log)'
 
@@ -39,7 +59,7 @@ TEST_NAME_REGEX = r'name=(.*log)'
 ########################################
 
 
-def create_instance(ami, sg_id):
+def create_instance(ami, sg_id, tags):
     """
     Given AMI, create and return an AWS EC2 instance object
     :param ami: AMI image name to launch the instance with
@@ -52,8 +72,51 @@ def create_instance(ami, sg_id):
     # Wait for the instance to enter the running state
     # Reload the instance attributes
 
+    # ec2 = boto3.client('ec2')
+
+    # Create the EC2 instance
+    response = ec2.create_instances(
+        ImageId=ami,
+        MinCount=1,
+        MaxCount=1,
+        SecurityGroupIds=[sg_id],  # Attach the security group
+        InstanceType=INSTANCE_TYPE,  # Specify instance type as needed
+        KeyName='my_key_pair',  # Specify your key pair
+    )
+
+    # Get the instance ID of the newly created instance
+    instance_id = response[0].id
+
+    # Reload the instance attributes
+    instance = ec2.Instance(instance_id)
+
+    # Wait for the instance to enter the running state
+    instance.wait_until_running()
+
+    instance.create_tags(Tags=tags)
+
     return instance
 
+def create_security_group(group_name, permissions):
+    # Check if security group with this name already exists
+    ec2_client = boto3.client('ec2')
+    security_groups = ec2_client.describe_security_groups(Filters=[
+        {
+            'Name': 'group-name',
+            'Values': [group_name]
+        }
+    ])['SecurityGroups']
+
+    if security_groups:
+        print(f"Security group {group_name} already exists.")
+        return security_groups[0]['GroupId']
+    
+    security_group = ec2.create_security_group(
+        GroupName=group_name,
+        Description='Security Group for ' + group_name
+        )
+    security_group.authorize_ingress(IpPermissions=permissions)
+    return security_group.id
 
 def initialize_test(lg_dns, first_web_service_dns):
     """
@@ -70,13 +133,16 @@ def initialize_test(lg_dns, first_web_service_dns):
     while not response or response.status_code != 200:
         try:
             response = requests.get(add_ws_string)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                log_link = soup.find('a')['href']
+                log_file_name = log_link.split('=')[-1]
+                return log_file_name
         except requests.exceptions.ConnectionError:
             time.sleep(1)
             pass 
 
-    # TODO: return log File name
-    return ""
-
+    return None
 
 def print_section(msg):
     """
@@ -126,7 +192,7 @@ def add_web_service_instance(lg_dns, sg2_name, log_name):
     :param sg2_name: name of WS security group
     :param log_name: name of the log file
     """
-    ins = create_instance(WEB_SERVICE_AMI, sg2_name)
+    ins = create_instance(WEB_SERVICE_AMI, sg2_name, WS_TAGS)
     print("New WS launched. id={}, dns={}".format(
         ins.instance_id,
         ins.public_dns_name)
@@ -227,34 +293,67 @@ def main():
     ]
 
     # TODO: Create two separate security groups and obtain the group ids
-    sg1_id = None  # Security group for Load Generator instances
-    sg2_id = None  # Security group for Web Service instances
+    sg1_id = create_security_group("Load Generator Security Group", sg_permissions)  # Security group for Load Generator instances
+    sg2_id = create_security_group("Web Service Security Group", sg_permissions)  # Security group for Web Service instances
 
     print_section('2 - create LG')
 
     # TODO: Create Load Generator instance and obtain ID and DNS
-    lg = ''
-    lg_id = ''
-    lg_dns = ''
+    lg = create_instance(LOAD_GENERATOR_AMI, sg1_id, LG_TAGS)
+    lg_id = lg.id
+    lg_dns = lg.public_dns_name
     print("Load Generator running: id={} dns={}".format(lg_id, lg_dns))
 
     print_section('3. Authenticate with the load generator')
     authenticate(lg_dns, SUBMISSION_PASSWORD, SUBMISSION_USERNAME)
 
     # TODO: Create First Web Service Instance and obtain the DNS
-    web_service_dns = ''
+    ws_instance = create_instance(WEB_SERVICE_AMI, sg2_id, WS_TAGS)
+    web_service_dns = ws_instance.public_dns_name
 
     print_section('4. Submit the first WS instance DNS to LG, starting test.')
     log_name = initialize_test(lg_dns, web_service_dns)
     last_launch_time = get_test_start_time(lg_dns, log_name)
+    start_time = get_test_start_time(lg_dns, log_name)
+    RPS_THRESHOLD = 50
+    MAX_TEST_DURATION = 30
+    COOLDOWN_PERIOD = 100
     while not is_test_complete(lg_dns, log_name):
         # TODO: Check RPS and last launch time
         # TODO: Add New Web Service Instance if Required
+        current_rps = get_rps(lg_dns, log_name)
+        current_time = datetime.datetime.utcnow()
+        last_launch_time_naive = last_launch_time.replace(tzinfo=None)
+        time_since_last_launch = current_time - last_launch_time_naive
+
+        start_time_naive = start_time.replace(tzinfo=None)
+        test_duration = current_time - start_time_naive
+
+        if test_duration > datetime.timedelta(minutes=MAX_TEST_DURATION):
+            print("Test exceeded the maximum duration of 30 minutes.")
+            break  
+
+        if current_rps > RPS_THRESHOLD:
+            print("Current RPS exceeded the RPS threshold.")
+            break  
+
+        if time_since_last_launch > datetime.timedelta(seconds=COOLDOWN_PERIOD):
+            add_web_service_instance(lg_dns, sg2_id, log_name)
+            last_launch_time = current_time
         time.sleep(1)
 
     print_section('End Test')
 
     # TODO: Terminate Resources
+    lg.terminate()
+
+    web_service_instances = ec2.instances.filter(Filters=[{
+        'Name': 'instance.group-id',
+        'Values': [sg2_id]
+    }])
+
+    for instance in web_service_instances:
+        instance.terminate()
 
 
 if __name__ == '__main__':
